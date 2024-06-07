4b3e218687a840efd98c06eb1a428c35ce2df8eb
risamba <4622117@ed.tus.ac.jp>
2024-06-07 21:10:00 +0900

    second

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+function DisplayDate() {
+    let now = new Date();
+    //年を取得する
+    let YYYY = now.getFullYear();
+    //月を取得する
+    let MM = now.getMonth()+1;
+    //日を取得する
+    let DD = now.getDate();
+    
+    alert(YYYY + "/" + MM + "/" + DD);
+  }
