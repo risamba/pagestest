bdd5b93a7740b228469d3800b215af1858cf37be
risamba <4622117@ed.tus.ac.jp>
2024-06-07 21:19:05 +0900

    third

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-function DisplayDate() {
-    let now = new Date();
-    //年を取得する
-    let YYYY = now.getFullYear();
-    //月を取得する
-    let MM = now.getMonth()+1;
-    //日を取得する
-    let DD = now.getDate();
-    
-    alert(YYYY + "/" + MM + "/" + DD);
-  }
+console.log('Hello World');
